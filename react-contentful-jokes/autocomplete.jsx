e6e99b6bd51a35965d4dab439f305a919a10bdf7
jonjaffe <jonjaffe5@gmail.com>
2018-01-19 10:29:21 -0800

    Starts basic styling

--- react-contentful-jokes/autocomplete.jsx
+++ react-contentful-jokes/autocomplete.jsx
@@ -13,7 +13,9 @@ export default class AutoComplete extends React.Component {
     this.state = {
       inputVal: '',
       joke: null,
-      sports: []
+      sports: [],
+      css_class: 'loader',
+      hidden_class: ''
     };
     this.selectSport = this.selectSport.bind(this);
   }
@@ -27,6 +29,7 @@ export default class AutoComplete extends React.Component {
   }
 
   selectSport(event) {
+    this.setState({css_class: 'loader'})
     let sport = event.currentTarget.innerText;
     contentfulClient.getEntries({
       content_type: 'joke',
@@ -36,7 +39,9 @@ export default class AutoComplete extends React.Component {
       let joke
       if (data.items.length) joke = data.items[Math.floor(Math.random() * data.items.length)].fields
       else joke = { body:"sorry no joke for you" }
-      this.setState({ joke: joke });
+      this.setState({ joke: joke }, () => {
+        setTimeout(() => this.setState({css_class: "", hidden_class: ""}), 750)
+      });
     })
   }
 
@@ -45,13 +50,12 @@ export default class AutoComplete extends React.Component {
       return (<li key={i} onClick={this.selectSport}>{result}</li>);
     });
       return (<div>
-        <h1>Click a sport to get a joke</h1>
+        <h1 className='header_text'>Click a sport to get a joke</h1>
         <div className='auto'>
-          <ul>
+          <ul className='sports_list'>
             {results}
           </ul>
-        </div>
-        <div>{!this.state.joke ? '' : <ReactMarkdown source={this.state.joke.body}/>}</div>
+        </div>{!this.state.joke ? '' : <div><div className='joke_header_text animated bounceInLeft'>Here's a great {this.state.joke.sport.fields.name.toLowerCase()} joke!</div><ReactMarkdown className={`joke_body ${this.state.css_class}`} source={this.state.joke.body}/></div>}
       </div>);
   }
 };
